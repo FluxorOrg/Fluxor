894eeec7fd49d82bd7d6b40ccddccbf473558041
Morten Bjerg Gregersen <morten@mogee.dk>
2020-01-03 00:09:15 +0100

    Update StoreInterceptor.swift

--- Sources/Fluxor/StoreInterceptor.swift
+++ Sources/Fluxor/StoreInterceptor.swift
@@ -4,11 +4,14 @@
  *  MIT license, see LICENSE file for details
  */
 
+/// A type which intercepts all `Action`s and the  `State` changes happening in a `Store`.
 public protocol StoreInterceptor {
     associatedtype State
+    /// The function called when an `Action` is dispatched on a `Store`.
     func actionDispatched(action: Action, oldState: State, newState: State)
 }
 
+/// A type-erased `StoreInterceptor` used to store all `StoreInterceptor`s in an array in the `Store`.
 struct AnyStoreInterceptor<State>: StoreInterceptor {
     private let _actionDispatched: (Action, State, State) -> Void
 
@@ -21,6 +24,7 @@ struct AnyStoreInterceptor<State>: StoreInterceptor {
     }
 }
 
+/// A `StoreInterceptor` to use in unit tests, to assert specific `Action`s are dispatched.
 public class TestStoreInterceptor<State>: StoreInterceptor {
     public private(set) var dispatchedActionsAndStates: [(action: Action, oldState: State, newState: State)] = []
 
