f4b484109053b76e9c3969d3a5f8c63f593cb1c4
Morten Bjerg Gregersen <morten@mogee.dk>
2020-01-03 00:05:45 +0100

    Update Publisher+OfType.swift

--- Sources/Fluxor/Publisher+OfType.swift
+++ Sources/Fluxor/Publisher+OfType.swift
@@ -7,6 +7,17 @@
 import Combine
 
 extension Publisher where Output == Action {
+    /**
+     Only lets actions of a certain type get through the stream.
+
+         actions
+             .ofType(FetchTodosAction.self)
+             .sink(receiveValue: { action in
+                 print("This is a FetchTodosAction: \(action)")
+             })
+
+     - Parameter typeToMatch: A type of `Action`to match
+     */
     public func ofType<T>(_ typeToMatch: T.Type) -> AnyPublisher<T, Self.Failure> {
         compactMap { $0 as? T }.eraseToAnyPublisher()
     }
