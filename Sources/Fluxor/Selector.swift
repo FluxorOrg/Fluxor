17be9a9b7c9e1f9e72a1987bdc6d7a9dcf432c36
Morten Bjerg Gregersen <morten@mogee.dk>
2020-01-10 23:25:54 +0100

    Add tests

--- Sources/Fluxor/Selector.swift
+++ Sources/Fluxor/Selector.swift
@@ -20,19 +20,19 @@ public func createRootSelector<State, Value>(
 }
 
 public func createSelector<State, S1, R1, Value>(
-    selector1: S1, projector: @escaping (R1) -> Value
+    _ selector1: S1, _ projector: @escaping (R1) -> Value
 ) -> Selector1<State, S1, R1, Value> {
     return Selector1(selector1: selector1, projector: projector)
 }
 
 public func createSelector<State, S1, S2, R1, R2, Value>(
-    selector1: S1, selector2: S2, projector: @escaping (R1, R2) -> Value
+    _ selector1: S1, _ selector2: S2, _ projector: @escaping (R1, R2) -> Value
 ) -> Selector2<State, S1, S2, R1, R2, Value> {
     return Selector2(selector1: selector1, selector2: selector2, projector: projector)
 }
 
 public func createSelector<State, S1, S2, S3, R1, R2, R3, Value>(
-    selector1: S1, selector2: S2, selector3: S3, projector: @escaping (R1, R2, R3) -> Value
+    _ selector1: S1, _ selector2: S2, _ selector3: S3, _ projector: @escaping (R1, R2, R3) -> Value
 ) -> Selector3<State, S1, S2, S3, R1, R2, R3, Value> {
     return Selector3(selector1: selector1, selector2: selector2, selector3: selector3, projector: projector)
 }
@@ -50,7 +50,7 @@ public struct RootSelector<State, Value>: Selector {
 public struct Selector1<State, S1, R1, Value>: Selector where
     S1: Selector, S1.State == State, S1.Value == R1 {
     internal let selector1: S1
-    internal let projector: (R1) -> Value
+    public let projector: (R1) -> Value
 
     public func map(_ state: State) -> Value {
         return projector(selector1.map(state))
@@ -62,7 +62,7 @@ public struct Selector2<State, S1, S2, R1, R2, Value>: Selector where
     S2: Selector, S2.State == State, S2.Value == R2 {
     internal let selector1: S1
     internal let selector2: S2
-    internal let projector: (R1, R2) -> Value
+    public let projector: (R1, R2) -> Value
 
     public func map(_ state: State) -> Value {
         return projector(selector1.map(state), selector2.map(state))
@@ -76,7 +76,7 @@ public struct Selector3<State, S1, S2, S3, R1, R2, R3, Value>: Selector where
     internal let selector1: S1
     internal let selector2: S2
     internal let selector3: S3
-    internal let projector: (R1, R2, R3) -> Value
+    public let projector: (R1, R2, R3) -> Value
 
     public func map(_ state: State) -> Value {
         return projector(selector1.map(state), selector2.map(state), selector3.map(state))
--- Tests/FluxorTests/SelectorTests.swift
+++ Tests/FluxorTests/SelectorTests.swift
@@ -10,21 +10,77 @@ import XCTest
 class SelectorTests: XCTestCase {
     private let state = TestState(name: NameState(firstName: "Tim",
                                                   lastName: "Cook"),
-                                  age: AgeState(years: 59,
-                                                months: 3),
-                                  address: AddressState(address: "One Apple Park Way,",
+                                  birthday: BirthdayState(year: 1960,
+                                                          month: "November",
+                                                          day: 1),
+                                  address: AddressState(address: "One Apple Park Way",
                                                         city: "Cupertino",
                                                         country: "USA"))
+    private let nameSelector = createRootSelector(keyPath: \TestState.name)
+    private let birthdaySelector = createRootSelector(keyPath: \TestState.birthday)
+    private let addressSelector = createRootSelector(keyPath: \TestState.address)
+
+    private lazy var fullNameSelector = createSelector(nameSelector) {
+        "\($0.firstName) \($0.lastName)"
+    }
+
+    private lazy var formattedBirthdaySelector = createSelector(birthdaySelector) {
+        "\($0.month) \($0.day), \($0.year)"
+    }
+
+    private lazy var formattedAddressSelector = createSelector(addressSelector) {
+        "\($0.address), \($0.city), \($0.country)"
+    }
 
     func testCreateRootSelector() {
-        let nameSelector = createRootSelector(keyPath: \TestState.name)
         XCTAssertEqual(nameSelector.map(state), state.name)
+        XCTAssertEqual(birthdaySelector.map(state), state.birthday)
+        XCTAssertEqual(addressSelector.map(state), state.address)
+    }
+
+    func testCreateSelector1() {
+        XCTAssertEqual(fullNameSelector.map(state), "Tim Cook")
+        let name = NameState(firstName: "Steve", lastName: "Jobs")
+        XCTAssertEqual(fullNameSelector.projector(name), "Steve Jobs")
+    }
+
+    func testCreateSelector2() {
+        let congratulationsSelector = createSelector(fullNameSelector, birthdaySelector) {
+            fullName, birthday in
+            "Congratulations \(fullName)! Today is \(birthday.month) \(birthday.day) - your birthday!"
+        }
+        XCTAssertEqual(congratulationsSelector.map(state),
+                       "Congratulations Tim Cook! Today is November 1 - your birthday!")
+        let birthday = BirthdayState(year: 1955, month: "February", day: 24)
+        XCTAssertEqual(congratulationsSelector.projector("Steve Jobs", birthday),
+                       "Congratulations Steve Jobs! Today is February 24 - your birthday!")
+    }
+
+    func testCreateSelector3() {
+        let bioSelector = createSelector(fullNameSelector, formattedBirthdaySelector, formattedAddressSelector) {
+            fullName, formattedBirthday, formattedAddress in
+            """
+            Full name: \(fullName)
+            Birthday: \(formattedBirthday)
+            Work address: \(formattedAddress)
+            """
+        }
+        XCTAssertEqual(bioSelector.map(state), """
+        Full name: Tim Cook
+        Birthday: November 1, 1960
+        Work address: One Apple Park Way, Cupertino, USA
+        """)
+        XCTAssertEqual(bioSelector.projector("Steve Jobs", "February 24, 1955", "One Infinite Loop, Cupertino, USA"), """
+        Full name: Steve Jobs
+        Birthday: February 24, 1955
+        Work address: One Infinite Loop, Cupertino, USA
+        """)
     }
 }
 
 private struct TestState: Equatable {
     let name: NameState
-    let age: AgeState
+    let birthday: BirthdayState
     let address: AddressState
 }
 
@@ -33,9 +89,10 @@ private struct NameState: Equatable {
     let lastName: String
 }
 
-private struct AgeState: Equatable {
-    let years: Int
-    let months: Int
+private struct BirthdayState: Equatable {
+    let year: Int
+    let month: String
+    let day: Int
 }
 
 private struct AddressState: Equatable {
