235870d9ed76754230d9f03108ada4d0fc4adc32
Morten Bjerg Gregersen <morten@mogee.dk>
2020-01-11 23:35:42 +0100

    Add documentation for Selectors

--- Sources/Fluxor/Selector.swift
+++ Sources/Fluxor/Selector.swift
@@ -4,53 +4,100 @@
  *  MIT license, see LICENSE file for details
  */
 
-/// A pure function which takes a `State` and returns a slice of it.
+/// A pure function which takes a `State` and returns a `Value` based on it.
 public protocol Selector {
     associatedtype State
     associatedtype Value
+    /// The function called when selecting from a `Store`.
     func map(_ state: State) -> Value
 }
 
-// MARK: - Functions
-
+/**
+ Creates a `RootSelector` from a `keyPath`.
+ 
+ - Parameter keyPath: The `keyPath` to create `RootSelector` from
+ */
 public func createRootSelector<State, Value>(
     keyPath: KeyPath<State, Value>
 ) -> RootSelector<State, Value> {
     return RootSelector(keyPath: keyPath)
 }
 
+/**
+ Creates a `Selector1` from a `Selector`and a `projector` function.
+ 
+ - Parameter selector1: The first `Selector`
+ - Parameter projector: The closure to pass the value from the `Selector` to
+ */
 public func createSelector<State, S1, Value>(
     _ selector1: S1, _ projector: @escaping (S1.Value) -> Value
 ) -> Selector1<State, S1, Value> {
     return Selector1(selector1: selector1, projector: projector)
 }
 
+/**
+Creates a `Selector2` from two `Selector`s and a `projector` function.
+
+- Parameter selector1: The first `Selector`
+- Parameter selector2: The second `Selector`
+- Parameter projector: The closure to pass the values from the `Selectors` to
+*/
 public func createSelector<State, S1, S2, Value>(
     _ selector1: S1, _ selector2: S2, _ projector: @escaping (S1.Value, S2.Value) -> Value
 ) -> Selector2<State, S1, S2, Value> {
     return Selector2(selector1: selector1, selector2: selector2, projector: projector)
 }
 
+/**
+Creates a `Selector3` from three `Selector`s and a `projector` function.
+
+- Parameter selector1: The first `Selector`
+- Parameter selector2: The second `Selector`
+- Parameter selector3: The third `Selector`
+- Parameter projector: The closure to pass the values from the `Selectors` to
+*/
 public func createSelector<State, S1, S2, S3, Value>(
     _ selector1: S1, _ selector2: S2, _ selector3: S3, _ projector: @escaping (S1.Value, S2.Value, S3.Value) -> Value
 ) -> Selector3<State, S1, S2, S3, Value> {
     return Selector3(selector1: selector1, selector2: selector2, selector3: selector3, projector: projector)
 }
 
+/**
+Creates a `Selector4` from four `Selector`s and a `projector` function.
+
+- Parameter selector1: The first `Selector`
+- Parameter selector2: The second `Selector`
+- Parameter selector3: The third `Selector`
+- Parameter selector4: The fourth `Selector`
+- Parameter projector: The closure to pass the values from the `Selectors` to
+*/
 public func createSelector<State, S1, S2, S3, S4, Value>(
     _ selector1: S1, _ selector2: S2, _ selector3: S3, _ selector4: S4, _ projector: @escaping (S1.Value, S2.Value, S3.Value, S4.Value) -> Value
 ) -> Selector4<State, S1, S2, S3, S4, Value> {
     return Selector4(selector1: selector1, selector2: selector2, selector3: selector3, selector4: selector4, projector: projector)
 }
 
+/**
+Creates a `Selector5` from five `Selector`s and a `projector` function.
+
+- Parameter selector1: The first `Selector`
+- Parameter selector2: The second `Selector`
+- Parameter selector3: The third `Selector`
+- Parameter selector4: The fourth `Selector`
+- Parameter selector5: The fifth `Selector`
+- Parameter projector: The closure to pass the values from the `Selectors` to
+*/
 public func createSelector<State, S1, S2, S3, S4, S5, Value>(
     _ selector1: S1, _ selector2: S2, _ selector3: S3, _ selector4: S4, _ selector5: S5, _ projector: @escaping (S1.Value, S2.Value, S3.Value, S4.Value, S5.Value) -> Value
 ) -> Selector5<State, S1, S2, S3, S4, S5, Value> {
     return Selector5(selector1: selector1, selector2: selector2, selector3: selector3, selector4: selector4, selector5: selector5, projector: projector)
 }
 
-// MARK: - Types
-
+/**
+ A `Selector` to get one of the root states in the `State` with a `KeyPath`.
+ 
+ Use it as a starting point in one of the other `Selector`s.
+ */
 public struct RootSelector<State, Value>: Selector {
     internal let keyPath: KeyPath<State, Value>
 
@@ -59,6 +106,7 @@ public struct RootSelector<State, Value>: Selector {
     }
 }
 
+/// A `Selector` based on a one other `Selector`.
 public struct Selector1<State, S1, Value>: Selector where
     S1: Selector, S1.State == State {
     internal let selector1: S1
@@ -69,6 +117,7 @@ public struct Selector1<State, S1, Value>: Selector where
     }
 }
 
+/// A `Selector` based on a two other `Selector`.
 public struct Selector2<State, S1, S2, Value>: Selector where
     S1: Selector, S1.State == State,
     S2: Selector, S2.State == State {
@@ -81,6 +130,7 @@ public struct Selector2<State, S1, S2, Value>: Selector where
     }
 }
 
+/// A `Selector` based on a three other `Selector`.
 public struct Selector3<State, S1, S2, S3, Value>: Selector where
     S1: Selector, S1.State == State,
     S2: Selector, S2.State == State,
@@ -95,6 +145,7 @@ public struct Selector3<State, S1, S2, S3, Value>: Selector where
     }
 }
 
+/// A `Selector` based on a four  other `Selector`.
 public struct Selector4<State, S1, S2, S3, S4, Value>: Selector where
     S1: Selector, S1.State == State,
     S2: Selector, S2.State == State,
@@ -111,6 +162,7 @@ public struct Selector4<State, S1, S2, S3, S4, Value>: Selector where
     }
 }
 
+/// A `Selector` based on a five other `Selector`.
 public struct Selector5<State, S1, S2, S3, S4, S5, Value>: Selector where
     S1: Selector, S1.State == State,
     S2: Selector, S2.State == State,
