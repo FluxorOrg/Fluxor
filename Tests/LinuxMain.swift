fc17c2ed0c21cf0b5eef68f08adda1a58560cbc9
Morten Bjerg Gregersen <morten@mogee.dk>
2020-02-18 21:30:59 +0100

    Update test manifests

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,10 +1,14 @@
 import XCTest
-
 import FluxorTests
 
 var tests = [XCTestCaseEntry]()
 tests += ActionTests.allTests()
-tests += PublisherOfTypeTests.allTests()
-tests += InterceptorTests.allTests()
-tests += StoreTests.allTests()
+tests += MockStoreTests.allTests(),
+tests += SelectorTests.allTests(),
+tests += StoreTests.allTests(),
+tests += PrintInterceptorTests.allTests(),
+tests += TestInterceptorTests.allTests(),
+tests += ActionPublisherOfTypeTests.allTests(),
+tests += ActionPublisherWasCreatedByTests.allTests(),
+tests += ActionPublisherWithIdentifierTests.allTests(),
 XCTMain(tests)
