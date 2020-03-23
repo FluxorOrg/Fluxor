b9f87037a8ef023f5990d03bf2163d67a5f0d05b
Morten Bjerg Gregersen <morten@mogee.dk>
2020-03-23 23:10:33 +0100

    Split the meaning of Reducer and OnReduce

--- Sources/Fluxor/Reducer.swift
+++ Sources/Fluxor/Reducer.swift
@@ -14,15 +14,15 @@ public func createReducer<State>(_ reduce: @escaping (inout State, Action) -> Vo
     return Reducer(reduce: reduce)
 }
 
-public func createReducer<State>(_ reduceOns: Reducer<State>...) -> Reducer<State> {
+public func createReducer<State>(_ reduceOns: OnReduce<State>...) -> Reducer<State> {
     return Reducer<State> { state, action in
         reduceOns.forEach { $0.reduce(&state, action) }
     }
 }
 
 public func reduceOn<State, A: Action>(_ actionType: A.Type,
-                                       reduce: @escaping (inout State, A) -> Void) -> Reducer<State> {
-    return Reducer { state, action in
+                                       reduce: @escaping (inout State, A) -> Void) -> OnReduce<State> {
+    return OnReduce { state, action in
         guard let action = action as? A else { return }
         reduce(&state, action)
     }
@@ -30,8 +30,8 @@ public func reduceOn<State, A: Action>(_ actionType: A.Type,
 
 public func reduceOn<State, C: ActionCreator>(_ actionCreator: C,
                                               reduce: @escaping (inout State, C.ActionType) -> Void)
-    -> Reducer<State> {
-    return Reducer { state, action in
+    -> OnReduce<State> {
+    return OnReduce { state, action in
         guard let anonymousAction = action as? AnonymousAction,
             let action = anonymousAction.asCreated(by: actionCreator) else { return }
         reduce(&state, action)
@@ -40,5 +40,10 @@ public func reduceOn<State, C: ActionCreator>(_ actionCreator: C,
 
 /// A `Reducer` created from a `reduce` function.
 public struct Reducer<State> {
+    public let reduce: (inout State, Action) -> Void
+}
+
+/// A `OnReduce` created from a `reduce` function. This could be used when creating a `Reducer`.
+public struct OnReduce<State> {
     public let reduce: (inout State, Action) -> Void
 }
