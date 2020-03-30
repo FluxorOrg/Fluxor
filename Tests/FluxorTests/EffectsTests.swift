ab88630ab7a6b16e8af468f31a70a99cc5336812
Morten Bjerg Gregersen <morten@mogee.dk>
2020-03-30 14:27:58 +0200

    Add tests for Effects

---
+++ Tests/FluxorTests/EffectsTests.swift
@@ -0,0 +1,51 @@
+/**
+ * FluxorTests
+ *  Copyright (c) Morten Bjerg Gregersen 2020
+ *  MIT license, see LICENSE file for details
+ */
+
+import Combine
+@testable import Fluxor
+import XCTest
+
+class EffectsTests: XCTestCase {
+    @Published var action: Action = InitialAction()
+
+    func testEffectCreators() {
+        // Given
+        class TestEffects: Effects {
+            let notAnEffect = 42
+            let anEffect = createEffectCreator { $0.sink { print($0) } }
+        }
+        // When
+        let testEffects = TestEffects()
+        // Then
+        XCTAssertEqual(testEffects.effectCreators.count, 1)
+    }
+
+    func testCreateEffectCreatorFromPublisher() {
+        // Given
+        let createPublisher = { (actionPublisher: ActionPublisher) -> AnyPublisher<Action, Never> in
+            actionPublisher.filter { _ in true }.eraseToAnyPublisher()
+        }
+        // When
+        let effectCreator = createEffectCreator(createPublisher)
+        let effect = effectCreator.createEffect(actionPublisher: $action)
+        // Then
+        guard case .dispatching = effect else { XCTFail(); return }
+        XCTAssertTrue(effectCreator is DispathingEffectCreator)
+    }
+
+    func testCreateEffectCreatorFromCancellable() {
+        // Given
+        let createPublisher = { (actionPublisher: ActionPublisher) -> AnyCancellable in
+            actionPublisher.sink { print($0) }
+        }
+        // When
+        let effectCreator = createEffectCreator(createPublisher)
+        let effect = effectCreator.createEffect(actionPublisher: $action)
+        // Then
+        guard case .nonDispatching = effect else { XCTFail(); return }
+        XCTAssertTrue(effectCreator is NonDispathingEffectCreator)
+    }
+}
