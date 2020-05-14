68af57c9c3c3427961caa61cfd467d06763fa356
Morten Bjerg Gregersen <morten@mogee.dk>
2020-05-14 23:32:26 +0200

    Update Store+Binding.swift

--- Sources/FluxorSwiftUI/Store+Binding.swift
+++ Sources/FluxorSwiftUI/Store+Binding.swift
@@ -13,6 +13,12 @@ extension Store {
         return .init(store: self, selector: selector, actionTemplate: actionTemplate)
     }
 
+    public func binding<Value, UpdateValue>(get selector: Fluxor.Selector<State, Value>,
+                                            set actionTemplateForValue: @escaping (Value) -> ActionTemplate<UpdateValue>)
+        -> DynamicValueBinding<State, Value, UpdateValue> {
+        return .init(store: self, selector: selector, actionTemplateForValue: actionTemplateForValue)
+    }
+
     public func observe<Value>(_ selector: Fluxor.Selector<State, Value>) -> ObservableValue<State, Value> {
         return .init(store: self, selector: selector)
     }
