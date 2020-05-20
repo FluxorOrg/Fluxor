0f54788b5c3ca9cf504cbcbd3230f3c185744245
Morten Bjerg Gregersen <morten@mogee.dk>
2020-05-20 23:12:52 +0200

    Update ObservableValue.swift

--- Sources/FluxorSwiftUI/ObservableValue.swift
+++ Sources/FluxorSwiftUI/ObservableValue.swift
@@ -7,12 +7,12 @@
 import Combine
 import Fluxor
 
-public class ObservableValue<Value>: ObservableObject {
-    public private(set) var value: Value { willSet { objectWillChange.send() } }
+public class ObservableValue<T>: ObservableObject {
+    public private(set) var current: T { willSet { objectWillChange.send() } }
     private var cancellable: AnyCancellable!
 
-    public init<State: Encodable>(store: Store<State>, selector: Selector<State, Value>) {
-        self.value = store.selectCurrent(selector)
-        self.cancellable = store.select(selector).sink { self.value = $0 }
+    public init<State: Encodable>(store: Store<State>, selector: Selector<State, T>) {
+        self.current = store.selectCurrent(selector)
+        self.cancellable = store.select(selector).assign(to: \.current, on: self)
     }
 }
