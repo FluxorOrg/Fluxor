5ee7a659d3a32f2cdd45bb88aa3d947c22bad950
Morten Bjerg Gregersen <morten@mogee.dk>
2020-05-25 23:50:35 +0200

    Improve interface for running Effects (#65)

---
+++ Sources/FluxorTestSupport/EffectRunner.swift
@@ -0,0 +1,104 @@
+/*
+ * FluxorTestSupport
+ *  Copyright (c) Morten Bjerg Gregersen 2020
+ *  MIT license, see LICENSE file for details
+ */
+
+import Combine
+import Dispatch
+import Fluxor
+import XCTest
+
+public struct EffectRunner {
+    /**
+     Run the `Effect` with the specified `Action` and return the published `Action`s.
+
+     The `expectedCount` defines how many `Action`s the `Publisher` should publish before they are returned.
+     If the `Effect` is `.nonDispatching`, the `expectedCount` is ignored.
+
+     - Parameter effect: The `Effect` to run
+     - Parameter action: The `Action` to send to the `Effect`
+     - Parameter expectedCount: The count of `Action`s to wait for
+     - Returns: The `Action`s published by the `Effect` if it is dispatching
+     */
+    @discardableResult
+    public static func run(_ effect: Effect, with action: Action, expectedCount: Int = 1) throws -> [Action]? {
+        let actions = PassthroughSubject<Action, Never>()
+        let runDispatchingEffect = { (publisher: AnyPublisher<[Action], Never>) throws -> [Action] in
+            let recorder = ActionRecorder(expectedNumberOfActions: expectedCount)
+            publisher.subscribe(recorder)
+            actions.send(action)
+            return try recorder.waitForAllActions()
+        }
+        switch effect {
+        case .dispatchingOne(let effectCreator):
+            return try runDispatchingEffect(effectCreator(actions.eraseToAnyPublisher())
+                .map { [$0] }.eraseToAnyPublisher())
+        case .dispatchingMultiple(let effectCreator):
+            return try runDispatchingEffect(effectCreator(actions.eraseToAnyPublisher()))
+        case .nonDispatching(let effectCreator):
+            var cancellables: [AnyCancellable] = []
+            effectCreator(actions.eraseToAnyPublisher()).store(in: &cancellables)
+            actions.send(action)
+            return nil
+        }
+    }
+}
+
+/**
+ The `ActionRecorder` records published `Action`s from a `Publisher`.
+
+ Inspired by: https://vojtastavik.com/2019/12/11/combine-publisher-blocking-recorder/
+ */
+private class ActionRecorder {
+    typealias Input = [Action]
+    typealias Failure = Never
+    private let expectation = XCTestExpectation()
+    private(set) var actions = [Action]() { didSet { expectation.fulfill() } }
+
+    /**
+     Initializes an `ActionRecorder` with the given `expectedNumberOfActions`.
+
+     - Parameter expectedNumberOfActions: The number of `Action`s to wait for
+     */
+    init(expectedNumberOfActions: Int) {
+        expectation.expectedFulfillmentCount = expectedNumberOfActions
+    }
+
+    /**
+     Wait for all the expected `Action`s to be published.
+
+     - Parameter timeout: The time waiting for the `Action`s
+     - Returns: The `Actions` recorded
+     */
+    func waitForAllActions(timeout: TimeInterval = 1) throws -> [Action] {
+        guard actions.count < expectation.expectedFulfillmentCount else { return actions }
+        let waitResult = XCTWaiter().wait(for: [expectation], timeout: timeout)
+        guard waitResult != .completed else { return actions }
+
+        let valueFormatter = { (count: Int) in "\(count) action" + (count == 1 ? "" : "s") }
+        let formattedExpectedActions = valueFormatter(expectation.expectedFulfillmentCount)
+        let formattedActions = valueFormatter(actions.count)
+        let errorMessage = "Waiting for \(formattedExpectedActions) timed out. Received only \(formattedActions)."
+        throw RecordingError.expectedCountNotReached(message: errorMessage)
+    }
+
+    enum RecordingError: Error {
+        case expectedCountNotReached(message: String)
+    }
+}
+
+extension ActionRecorder: Subscriber {
+    func receive(subscription: Subscription) {
+        subscription.request(.unlimited)
+    }
+
+    func receive(_ input: Input) -> Subscribers.Demand {
+        DispatchQueue.main.async {
+            input.forEach { self.actions.append($0) }
+        }
+        return .unlimited
+    }
+
+    func receive(completion: Subscribers.Completion<Never>) {}
+}
--- Tests/FluxorTests/EffectsTests.swift
+++ Tests/FluxorTests/EffectsTests.swift
@@ -6,6 +6,7 @@
 
 import Combine
 @testable import Fluxor
+import FluxorTestSupport
 import XCTest
 
 // swiftlint:disable nesting force_cast
@@ -29,64 +30,70 @@ class EffectsTests: XCTestCase {
     /// Can we run a single dispatching `Effect`?
     func testEffectRunDispatchingOne() throws {
         // Given
+        let action1 = Test1Action()
         let action2 = Test2Action()
         let expectation = XCTestExpectation(description: debugDescription)
         expectation.expectedFulfillmentCount = 1
         let effect = Effect.dispatchingOne {
             $0.ofType(Test1Action.self)
-                .map { _ in
+                .map {
+                    XCTAssertEqual($0, action1)
                     expectation.fulfill()
                     return action2
                 }
                 .eraseToAnyPublisher()
         }
         // When
-        let action = Test1Action()
-        let actions: [Action] = try effect.run(with: action)
-        effect.run(with: action) // Returns early because of wrong type
+        let actions = try EffectRunner.run(effect, with: action1)!
         // Then
         wait(for: [expectation], timeout: 1)
+        XCTAssertEqual(actions.count, 1)
         XCTAssertEqual(actions[0] as! Test2Action, action2)
-        XCTAssertThrowsError(try effect.run(with: action, expectedCount: 2))
+        XCTAssertThrowsError(try EffectRunner.run(effect, with: action1, expectedCount: 2))
     }
 
     /// Can we run a multi dispatching `Effect`?
     func testEffectRunDispatchingMultiple() throws {
         // Given
+        let action1 = Test1Action()
         let action2 = Test2Action()
         let action3 = Test3Action()
         let expectation = XCTestExpectation(description: debugDescription)
         expectation.expectedFulfillmentCount = 1
         let effect = Effect.dispatchingMultiple {
             $0.ofType(Test1Action.self)
-                .map { _ in
+                .map {
+                    XCTAssertEqual($0, action1)
                     expectation.fulfill()
                     return [action2, action3]
                 }
                 .eraseToAnyPublisher()
         }
         // When
-        let action = Test1Action()
-        let actions: [Action] = try effect.run(with: action, expectedCount: 2)
-        effect.run(with: action) // Returns early because of wrong type
+        let actions = try EffectRunner.run(effect, with: action1, expectedCount: 2)!
         // Then
         wait(for: [expectation], timeout: 1)
+        XCTAssertEqual(actions.count, 2)
         XCTAssertEqual(actions[0] as! Test2Action, action2)
         XCTAssertEqual(actions[1] as! Test3Action, action3)
+        XCTAssertThrowsError(try EffectRunner.run(effect, with: action1, expectedCount: 3))
     }
 
     /// Can we run a non dispatching `Effect`?
     func testEffectRunNonDispatching() throws {
         // Given
         let expectation = XCTestExpectation(description: debugDescription)
-        expectation.expectedFulfillmentCount = 1
+        expectation.expectedFulfillmentCount = 2
+        let action = Test1Action()
         let effect = Effect.nonDispatching {
-            $0.sink { _ in expectation.fulfill() }
+            $0.sink {
+                XCTAssertEqual($0 as! Test1Action, action)
+                expectation.fulfill()
+            }
         }
         // When
-        let action = Test1Action()
-        effect.run(with: action)
-        XCTAssertThrowsError(try effect.run(with: action, expectedCount: 1)) // Returns early because of wrong type
+        XCTAssertNil(try EffectRunner.run(effect, with: action))
+        XCTAssertNil(try EffectRunner.run(effect, with: action, expectedCount: 9))
         // Then
         wait(for: [expectation], timeout: 1)
     }
@@ -102,12 +109,14 @@ class EffectsTests: XCTestCase {
             }
             return publisher.eraseToAnyPublisher()
         }
-        _ = try effect.run(with: Test1Action(), expectedCount: 1)
+        try EffectRunner.run(effect, with: Test1Action(), expectedCount: 1)
         XCTAssertNotNil(cancellable)
     }
 }
 
-private struct Test1Action: Action {}
+private struct Test1Action: Action, Equatable {
+    let id = UUID()
+}
 
 private struct Test2Action: Action, Equatable {
     let id = UUID()
