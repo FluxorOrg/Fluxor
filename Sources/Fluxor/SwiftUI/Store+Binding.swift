6ee6e28b0cf38ffa5aa859ee32b248d4300d2d43
Morten Bjerg Gregersen <morten@mogee.dk>
2021-10-23 22:42:25 +0200

    Move and clean up Store+Binding

--- Sources/Fluxor/SwiftUI/Store+Binding.swift
+++ Sources/Fluxor/SwiftUI/Store+Binding.swift
@@ -1,25 +1,24 @@
-/*
- * FluxorSwiftUI
+/**
+ * Fluxor
  *  Copyright (c) Morten Bjerg Gregersen 2020
  *  MIT license, see LICENSE file for details
  */
 
 #if canImport(SwiftUI)
 
-import Fluxor
 import SwiftUI
 
 public extension Store {
     /**
-     Creates a `ValueBinding` from the given `Selector` and `ActionTemplate`.
-
-      When the value is updated an `Action`, created from the `ActionTemplate`, is dispatched on the `Store`.
+     Creates a `Binding` from the given `Selector` and `ActionTemplate`.
+     
+     When the `wrappedValue` is updated an `Action`, created from the `ActionTemplate`, is dispatched on the `Store`.
 
      - Parameter selector: The `Selector`s to use for getting the current value
      - Parameter actionTemplate: The `ActionTemplate` to use for dispatching an `Action` when the value changes
      - Returns: A `Binding` based on the given `Selector` and `ActionTemplate`
      */
-    func binding<Value>(get selector: Fluxor.Selector<State, Value>,
+    func binding<Value>(get selector: Selector<State, Value>,
                         send actionTemplate: ActionTemplate<Value>) -> Binding<Value> {
         .init(get: { self.selectCurrent(selector) },
               set: { self.dispatch(action: actionTemplate.createAction(payload: $0)) })
@@ -28,17 +27,15 @@ public extension Store {
     /**
      Creates a `Binding` from the given `Selector` and `ActionTemplate`s for enabling and disabling the value.
 
-     When the value is enabled/disabled, an `Action`, created from one of the `ActionTemplate`s,
+     When the `wrappedValue` is enabled/disabled, an `Action`, created from one of the `ActionTemplate`s,
      is dispatched on the `Store`.
 
      - Parameter selector: The `Selector`s to use for getting the current value
-     - Parameter enableActionTemplate: The `ActionTemplate` to use for dispatching an `Action`
-                                       when the value should be enabled
-     - Parameter disableActionTemplate: The `ActionTemplate` to use for dispatching an `Action`
-                                        when the value should be disabled
+     - Parameter enableActionTemplate: The `ActionTemplate` to use for dispatching an `Action` when the value should be enabled
+     - Parameter disableActionTemplate: The `ActionTemplate` to use for dispatching an `Action` when the value should be disabled
      - Returns: A `Binding` based on the given `Selector` and `ActionTemplate`s
      */
-    func binding(get selector: Fluxor.Selector<State, Bool>,
+    func binding(get selector: Selector<State, Bool>,
                  enable enableActionTemplate: ActionTemplate<Void>,
                  disable disableActionTemplate: ActionTemplate<Void>)
         -> Binding<Bool> {
@@ -47,18 +44,16 @@ public extension Store {
     }
 
     /**
-     Creates a `Binding` from the given `Selector` and `ActionTemplate`.
+     Creates a `Binding` from the given `Selector` and closure.
 
-      When the value is updated an `Action` (returned by the closure), is dispatched on the `Store`.
+     When the `wrappedValue` is updated an `Action` (returned by the closure), is dispatched on the `Store`.
 
      - Parameter selector: The `Selector`s to use for getting the current value
-     - Parameter action: A closure used to decide which `ActionTemplate` to use
-                                 for dispatching an `Action` when the value changes
-     - Parameter value: The value used to decide which `ActionTemplate` to use for the update.
-                        This can either be the current value or the one used for the update
-     - Returns: A `Binding` based on the given `Selector` and `ActionTemplate`
+     - Parameter action: A closure which returns an `Action` to be dispatched when the value changes
+     - Parameter value: The value used to decide which `Action` to be dispatched.
+     - Returns: A `Binding` based on the given `Selector` and closure
      */
-    func binding<Value>(get selector: Fluxor.Selector<State, Value>,
+    func binding<Value>(get selector: Selector<State, Value>,
                         send action: @escaping (_ value: Value) -> Action)
         -> Binding<Value> {
         return .init(get: { self.selectCurrent(selector) },
