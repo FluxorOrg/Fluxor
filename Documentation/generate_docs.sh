cf000696243f9c3cb93eeb20ba0a339ad6cf5dd2
Morten Bjerg Gregersen <morten@mogee.dk>
2021-10-23 22:12:46 +0200

    Remove FluxorSwiftUI

--- Documentation/generate_docs.sh
+++ Documentation/generate_docs.sh
@@ -2,6 +2,5 @@ rm -rf docs
 mkdir docs
 sourcekitten doc --spm --module-name Fluxor > docs/mod1.json
 sourcekitten doc --spm --module-name FluxorTestSupport > docs/mod2.json
-sourcekitten doc --spm --module-name FluxorSwiftUI > docs/mod3.json
-jazzy --sourcekitten-sourcefile docs/mod1.json,docs/mod2.json,docs/mod3.json
+jazzy --sourcekitten-sourcefile docs/mod1.json,docs/mod2.json
 open docs/index.html
