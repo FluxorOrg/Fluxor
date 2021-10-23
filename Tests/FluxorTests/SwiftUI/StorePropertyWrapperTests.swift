b0190979bcf43fe12db169c5650f72d4c1560975
Morten Bjerg Gregersen <morten@mogee.dk>
2021-10-24 00:08:24 +0200

    Update StorePropertyWrapperTests.swift

--- Tests/FluxorTests/SwiftUI/StorePropertyWrapperTests.swift
+++ Tests/FluxorTests/SwiftUI/StorePropertyWrapperTests.swift
@@ -4,19 +4,21 @@
  *  MIT license, see LICENSE file for details
  */
 
+#if canImport(SwiftUI)
+
 @testable import Fluxor
 import XCTest
 
 final class StorePropertyWrapperTests: XCTestCase {
     func testAddStore() throws {
-        let store = Store(initialState: 42)
+        let store = Store(initialState: TestState(counter: 42))
         StorePropertyWrapper.addStore(store)
-        let savedStore: AnyEnvironmentStore<Int> = try StorePropertyWrapper.getStore()
-        XCTAssertEqual(savedStore.selectCurrent(Selector(keyPath: \.self)), 42)
+        let savedStore: AnyEnvironmentStore<TestState> = try StorePropertyWrapper.getStore()
+        XCTAssertEqual(savedStore.selectCurrent(Selector(keyPath: \TestState.counter)), 42)
     }
 
     func testStoreNotAdded() throws {
-        let store = Store(initialState: 42)
+        let store = Store(initialState: TestState(counter: 42))
         StorePropertyWrapper.addStore(store)
         XCTAssertThrowsError(try StorePropertyWrapper.getStore() as AnyEnvironmentStore<String>) { error in
             // swiftlint:disable:next force_cast
@@ -30,3 +32,9 @@ extension StorePropertyWrapperError: Equatable {
         lhs.localizedDescription == rhs.localizedDescription
     }
 }
+
+private struct TestState: Encodable {
+    let counter: Int
+}
+
+#endif
