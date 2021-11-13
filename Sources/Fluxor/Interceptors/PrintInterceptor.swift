ac32a1422c8d72375e28c2c524e573f2734f7277
Morten Bjerg Gregersen <morten@mogee.dk>
2021-11-13 23:42:53 +0100

    Prettier log for IdentifiableAction with PrintInterceptor (#103)
    * Prettier log for IdentifiableAction with PrintInterceptor
    
    * Update PrintInterceptorTests.swift
    
    * Refactor

--- Sources/Fluxor/Interceptors/PrintInterceptor.swift
+++ Sources/Fluxor/Interceptors/PrintInterceptor.swift
@@ -9,6 +9,7 @@ import Foundation
 /// A `Interceptor` to use when debugging. Every `Action`s and `State` change are printed to the console.
 public class PrintInterceptor<State: Encodable>: Interceptor {
     private let print: (String) -> Void
+    private var name: String { String(describing: type(of: self)) }
 
     /// Initializes the `PrintInterceptor`.
     public convenience init() {
@@ -29,26 +30,41 @@ public class PrintInterceptor<State: Encodable>: Interceptor {
     public func actionDispatched(action: Action, oldState: State, newState: State) {
         let encoder = JSONEncoder()
         encoder.outputFormatting = [.prettyPrinted, .sortedKeys]
-        let name = String(describing: type(of: self))
 
-        let actionName = String(describing: type(of: action))
+        let actionLog = getActionLog(for: action, encoder: encoder)
+        self.print(actionLog)
+
+        guard let stateLog = getStateLog(for: newState, encoder: encoder) else { return }
+        self.print(stateLog)
+    }
+
+    private func getActionId(for action: Action) -> String {
+        if let action = action as? IdentifiableAction {
+            return "\"\(action.id)\""
+        }
+        return String(describing: type(of: action))
+    }
+
+    private func getActionLog(for action: Action, encoder: JSONEncoder) -> String {
+        let actionName = getActionId(for: action)
         var actionLog = "\(name) - action dispatched: \(actionName)"
         if Mirror(reflecting: action).children.count > 0 {
             if let encodableAction = action as? EncodableAction,
-                let actionData = encodableAction.encode(with: encoder),
-                let actionJSON = String(data: actionData, encoding: .utf8),
-                actionJSON.replacingOccurrences(of: "\n", with: "") != "{}" {
+               let actionData = encodableAction.encode(with: encoder),
+               let actionJSON = String(data: actionData, encoding: .utf8),
+               actionJSON.replacingOccurrences(of: "\n", with: "") != "{}" {
                 actionLog += ", data: \(actionJSON)"
             } else {
                 actionLog += "\n⚠️ The payload of the Action has properties but aren't Encodable."
                 actionLog += " Make it Encodable to get them printed."
             }
         }
-        self.print(actionLog)
+        return actionLog
+    }
 
-        if let stateData = try? encoder.encode(newState),
-            let newStateJSON = String(data: stateData, encoding: .utf8) {
-            self.print("\(name) - state changed to: \(newStateJSON)")
-        }
+    private func getStateLog(for state: State, encoder: JSONEncoder) -> String? {
+        guard let stateData = try? encoder.encode(state),
+              let newStateJSON = String(data: stateData, encoding: .utf8) else { return nil }
+        return "\(name) - state changed to: \(newStateJSON)"
     }
 }
