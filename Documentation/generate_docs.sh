2c0bd336b71c9d522c6a16830b9a132bf043d8e2
Morten Bjerg Gregersen <morten@mogee.dk>
2021-11-25 22:16:05 +0100

    Hide light logo on website

--- Documentation/generate_docs.sh
+++ Documentation/generate_docs.sh
@@ -1,6 +1,7 @@
-rm -rf docs
-mkdir docs
-sourcekitten doc --spm --module-name Fluxor > docs/mod1.json
-sourcekitten doc --spm --module-name FluxorTestSupport > docs/mod2.json
-jazzy --sourcekitten-sourcefile docs/mod1.json,docs/mod2.json
+#rm -rf docs
+#mkdir docs
+#sourcekitten doc --spm --module-name Fluxor > docs/mod1.json
+#sourcekitten doc --spm --module-name FluxorTestSupport > docs/mod2.json
+jazzy --sourcekitten-sourcefile docs/mod1.json,docs/mod2.json --head '<link rel="stylesheet" type="text/css" href="css/fluxor.css">'
+cp Documentation/fluxor.css docs/css/fluxor.css
 open docs/index.html
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 <p align="center">
     <br />
     <img src="https://raw.githubusercontent.com/FluxorOrg/Fluxor/master/Assets/Fluxor-logo-light.png#gh-light-mode-only" width="400" max-width="90%" alt="Fluxor" />
-    <img src="https://raw.githubusercontent.com/FluxorOrg/Fluxor/master/Assets/Fluxor-logo-dark.png#gh-dark-mode-only" width="400" max-width="90%" alt="Fluxor" />
+    <img src="https://raw.githubusercontent.com/FluxorOrg/Fluxor/master/Assets/Fluxor-logo-dark.png#gh-dark-mode-only" id="dark-logo" width="400" max-width="90%" alt="Fluxor" />
 </p>
 
 <p align="center">
