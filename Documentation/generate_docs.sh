929a4b7ac08559036319b27d5206d4f166b07dda
Morten Bjerg Gregersen <morten@mogee.dk>
2021-11-25 22:21:19 +0100

    Hide light logo on website (#106)
    * Hide light logo on website
    
    * Update generate_docs.sh

--- Documentation/generate_docs.sh
+++ Documentation/generate_docs.sh
@@ -2,5 +2,6 @@ rm -rf docs
 mkdir docs
 sourcekitten doc --spm --module-name Fluxor > docs/mod1.json
 sourcekitten doc --spm --module-name FluxorTestSupport > docs/mod2.json
-jazzy --sourcekitten-sourcefile docs/mod1.json,docs/mod2.json
+jazzy --sourcekitten-sourcefile docs/mod1.json,docs/mod2.json --head '<link rel="stylesheet" type="text/css" href="css/fluxor.css">'
+cp Documentation/fluxor.css docs/css/fluxor.css
 open docs/index.html
